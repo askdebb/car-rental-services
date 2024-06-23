f18aeb25b1ff5a609c381bb527dd055b7cf54856
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-06-23 23:08:08 +0000

    updated the checkout flutterwave

---
+++ carRentals/showroom/forms.py
@@ -0,0 +1,19 @@
+# forms.py
+from django import forms
+from .models import Reservation
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = [
+            'name', 'id_number', 'phone_number', 'address', 
+            'emergency_contact_name', 'emergency_contact_phone', 
+            'reservation_type', 'delivery_method', 'company_name', 
+            'company_email', 'company_contact'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['company_name'].required = False
+        self.fields['company_email'].required = False
+        self.fields['company_contact'].required = False
